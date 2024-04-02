ef7a035b9dfe3da497c6cadb4a79aef7fbc12963
beilineili <43463041+beilineili@users.noreply.github.com>
2024-04-02 15:21:57 +0800

    Update normal_distribution.py

--- rca/normal_distribution.py
+++ rca/normal_distribution.py
@@ -13,7 +13,6 @@ if __name__=='__main__':
     with open("xxx/" + dataset + backbone + "_graphType2nodeScore_" + str(layers_num) + "layers_" + str(hidden_num) + "dim_" + str(epoch_num) + "epoch-" + time + ablation + ".json","r") as f:
         graphType2nodeScore = json.load(f)
         
-    # 保存每种图的每个节点的分数分布
     graphType2nodeDistribution = {}
     for graphType, node in graphType2nodeScore.items():
         graphType2nodeDistribution[graphType] = {}
