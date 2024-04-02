b3928b0b47b5040e271eb1c9c4c7e1d02f528f13
beilineili <43463041+beilineili@users.noreply.github.com>
2024-04-02 15:22:21 +0800

    Update normal_score.py

--- rca/normal_score.py
+++ rca/normal_score.py
@@ -11,34 +11,28 @@ if __name__=='__main__':
     dataset = "train-ticket"
     time = "1"
 
-# 加载模型
     with open("xxx/" + dataset + "-train" + backbone + "_model_" + str(layers_num) + "layers_" + str(hidden_num) + "dim_" + str(epoch_num) + "epoch-" + time + ablation + ".pkl", "rb") as f:
         loaded_model = pickle.load(f)
     
     with open('xxx/name2nodeNum_dict.json', 'r') as f:
         name2nodeNum_dict = json.load(f)
 
-# 保存每种图的每个节点的分数
     graphType2nodeScore = {}
     for key in name2nodeNum_dict.keys():
         graphType2nodeScore[key] = {}
 
-# 读取正常的数据集
     dataset_data_normal = []
     for i in range(0, 8):
         dataset_data_normal.append(MyOwnDataset(root="xxx/train-ticket-test-normal-" + ablation + str(i)))
-# 读取正常文件列表
     with open('xxx/normal_file_list.pkl', 'rb') as file:
         normal_file_list = pickle.load(file)
 
-# 分类后得到每个类的正常时的分数
     for index, dataset_data in enumerate(dataset_data_normal):
         data_size = len(dataset_data)
         for i in range(data_size):
             data = dataset_data[i] 
             fileName = normal_file_list[i + index*10000]
             trace_type = fileName.split("nodeAttr_dict_")[1].split("-000")[0]
-            # 返回预测结果 pred，得分 score
             pred, score, emb = loaded_model.predict(data, return_pred=True, return_score=True, return_emb=True)
             
             for j in range(data.num_nodes):
