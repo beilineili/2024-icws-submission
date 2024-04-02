4c4913d263e18c08fa0bb9cd247db25a7a1bee8b
beilineili <43463041+beilineili@users.noreply.github.com>
2024-04-02 15:21:39 +0800

    Update create_dataset.py

--- rca/create_dataset.py
+++ rca/create_dataset.py
@@ -16,25 +16,19 @@ class MyOwnDataset(InMemoryDataset):
     def processed_file_names(self):
         return ['data.pt']
     
-    # 生成数据集所用的方法
     def process(self):
         folder_path = 'xxx/vector/'
-        # 获取文件夹中的所有文件和文件夹列表
         file_list = os.listdir(folder_path)
         sorted_file_list = file_list
         data_list = []
         cnt = 0
-        # 遍历文件列表，每个文件做成一张图，然后放入图数据集
         for file_name in sorted_file_list:
             file_path = os.path.join(folder_path, file_name)
             # Read data into huge `Data` list.
             cnt += 1
 
-            # 这里用于构建datas
             X, Y, Edge_index = graph_construction(file_path)
-            # 放入datalist
             data = Data(x=X, y=Y, edge_index=Edge_index)
-            # 创建包含多个图的图数据集
             data_list.append(data)
 
         if self.pre_filter is not None:
@@ -47,11 +41,9 @@ class MyOwnDataset(InMemoryDataset):
         torch.save((data, slices), self.processed_paths[0])
 
 
-# 接受一个文件路径作为参数，并返回读取到的邻接矩阵。
 def read_adjacency_matrix(file_path):
     matrix = []
     with open(file_path, 'r') as file:
-        # 迭代读取每一行的方式，逐行处理邻接矩阵数据
         for line in file:
             row = list(map(int, line.strip().split()))
             matrix.append(row)
@@ -67,14 +59,12 @@ def graph_construction(file_path):
     
     with open(file_path, 'r') as f:
         nodeAttributeDict = json.load(f)
-    # 将每个节点的属性进行拼接，得到节点向量
     for nodeName in nodeAttributeDict.keys():
         node_embedding = []
         for key, value in nodeAttributeDict[nodeName].items():
             if "vector" in key:
                 node_embedding += value
         feature_list.append(node_embedding)
-        # 打标签
         if nodeAttributeDict[nodeName]["label"] == "normal":
             label_list.append(0)
         else:
@@ -83,16 +73,12 @@ def graph_construction(file_path):
         index += 1
         functionNameDict[nodeName.split("_")[0]] = True
     
-    # 每个节点的特征 
     X = torch.tensor(feature_list, dtype=torch.float)
-    # 每个节点的标签 
     Y = torch.tensor(label_list, dtype=torch.long)    
     nodeIndex2nodeNameDict = {value:key for key, value in nodeName2nodeIndexDict.items()}
     
-# 添加节点之间的边 
     forward_edge_list = []
     backward_edge_list = []
-    # 在单个函数内部添加平台侧组件节点的边，同一函数前缀的不同组件节点之间有连边
     nodeNameEnd = ["_deployment", "_replicaset", "_pod", "_container"]
     for functionName in functionNameDict.keys():
         for i in range(len(nodeNameEnd) - 1):
@@ -101,30 +87,25 @@ def graph_construction(file_path):
             forward_edge_list.append(from_node)
             backward_edge_list.append(to_node)
 
-# 从adjMatrix和serviceName2nodeIndex中提取边
     adjMatrix_path = "xxx/adjMatrix"
     nodeIndex_path = "xxx/serviceName2nodeIndex"
     processed_string = file_path.split("/vector/")[1]
     processed_string = processed_string.split("_vector")[0]
     adjMatrix_file = os.path.join(adjMatrix_path, "adjMatrix_"+processed_string+".txt")
     nodeIndex_file = os.path.join(nodeIndex_path, "serviceName2nodeIndex_"+processed_string+".json")
-    # 读取邻接矩阵和函数节点对应表
     adjacency_matrix = read_adjacency_matrix(adjMatrix_file)
     with open(nodeIndex_file, 'r') as f2:
         nodeIndex_data = json.load(f2)
     indexNode_data = {value:key for key, value in nodeIndex_data.items()}
-    # 根据 trace 逐个函数添加边
     for node, index in nodeIndex_data.items() :
         for col_index in range(len(adjacency_matrix[index])):
             element = adjacency_matrix[index][col_index]
-            # 如果存在调用则会有有向边，从调用的 container 到被调用的 deployment
             if element > 0 :
                 from_node = nodeName2nodeIndexDict[node + "_container"]
                 to_node = nodeName2nodeIndexDict[indexNode_data[col_index] + "_deployment"]
                 forward_edge_list.append(from_node)
                 backward_edge_list.append(to_node)
-
-    # 边的索引，[2,num_edges]从上指向下一一对应 
+                
     Edge_index = torch.tensor([forward_edge_list,
                                backward_edge_list], dtype=torch.long)
 
